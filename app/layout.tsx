b729211e2deec2a24a5815b8d790f1a59b5ab290
Ernest rewer <s70239176@gmail.com>
2026-07-30 23:33:40 +0000

    fix: move font to link tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,11 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+    <head>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+      <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&family=Space+Mono:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet" />
+    </head>
       <body>
         <Providers>
           <div style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
